3c4a552f55beb791c6a55fdba79fdedc59b55a73
Avazkhon <avazkhon@star-tex.ru>
2020-02-28 12:53:56 +0700

    init commit

---
+++ src/conteiner/Home/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="Home-header">
+          <h2>Welcome to omdapi</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
