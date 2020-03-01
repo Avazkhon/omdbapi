4486e958ad8198cb557b3c8155488b70544baa42
Avazkhon <kamalxanovavazxon@gmail.com>
2020-03-02 02:50:35 +0700

    update Header

--- src/containers/Header/index.js
+++ src/containers/Header/index.js
@@ -1,23 +1,25 @@
 import React from 'react';
-import { Nav } from 'react-bootstrap';
+import { Nav, Navbar } from 'react-bootstrap';
 
 const navBar = [
-  { id: 1, label: 'Главная', url: '/'},
-  { id: 3, label: 'Избранное', url: '/favorites'},
-]
+  { id: 1, label: 'Home', url: '/'},
+  { id: 3, label: 'Favorites', url: '/favorites'},
+];
 
 const Header = () => (
-  <Nav variant="tabs">
-    {
-      navBar.map((itm) => {
-        return (
-          <Nav.Item key={itm.id}>
-            <Nav.Link href={itm.url}>{itm.label}</Nav.Link>
-          </Nav.Item>
-        )
-      })
-    }
-  </Nav>
+  <Navbar bg="dark" variant="dark">
+    <Nav variant="tabs">
+      {
+        navBar.map((itm) => {
+          return (
+            <Nav.Item key={itm.id}>
+              <Nav.Link href={itm.url}>{itm.label}</Nav.Link>
+            </Nav.Item>
+          )
+        })
+      }
+    </Nav>
+  </Navbar>
 );
 
 export default Header;
