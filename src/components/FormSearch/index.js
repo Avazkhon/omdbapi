703e4470e6f7e3f0ed6f5e6248350097d67b8ec9
Avazkhon <kamalxanovavazxon@gmail.com>
2020-03-01 22:11:53 +0700

    create FormSearch components

---
+++ src/components/FormSearch/index.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  Form,
+  FormControl,
+  Button,
+  Container,
+  Row,
+  Col,
+} from 'react-bootstrap';
+
+
+
+class FormSearch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {
+        search: '',
+        typeVideo: '',
+        year: '',
+      },
+    }
+  }
+  componentDidMount() {
+    const { getFilmsInfo } = this.props;
+    if (typeof getFilmsInfo === 'function') {
+      getFilmsInfo();
+    }
+  }
+
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col sm="8">
+            <FormControl type="text" placeholder="Enter name" className="mr-sm-2" />
+          </Col>
+          <Col sm="2">
+            <Button variant="outline-success">Search</Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm="3">
+            <Form.Label>Type video</Form.Label>
+            <Form.Control as="select">
+              <option value="">All</option>
+              <option value="movie">Movie</option>
+              <option value="series">Series</option>
+              <option value="episode">Episode</option>
+            </Form.Control>
+          </Col>
+          <Col sm="3">
+            <Form.Label>Year of release. </Form.Label>
+            <Form.Control type="number" placeholder="Enter year" />
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+FormSearch.propTypes = {
+  getFilmsInfo: PropTypes.func,
+};
+
+export default FormSearch
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -10,52 +10,26 @@ import {
   Col,
 } from 'react-bootstrap';
 
-import Layout from '../Layout';
-
 import {
   getFilmsInfo
-} from '../../actions';
+} from 'actions';
+
+import Layout from '../Layout';
+
+
+import FormSearch from 'components/FormSearch';
 
 class Home extends React.Component {
-  componentDidMount() {
-    const { getFilmsInfo } = this.props;
-    if (typeof getFilmsInfo === 'function') {
-      getFilmsInfo();
-    }
-    // fetch('http://www.omdbapi.com/?t=joker&apikey=1977b733')
-    // .then((a) => {
-    //   console.log('a', a);
-    // })
-  }
 
   render() {
+    const {
+      getFilmsInfo,
+    } = this.props;
     return (
       <Layout>
-        <Container>
-          <Row>
-            <Col sm="8">
-            <FormControl type="text" placeholder="Enter name" className="mr-sm-2" />
-            </Col>
-            <Col sm="2">
-              <Button variant="outline-success">Search</Button>
-            </Col>
-          </Row>
-          <Row>
-            <Col sm="3">
-              <Form.Label>Type video</Form.Label>
-              <Form.Control as="select">
-                <option value="">All</option>
-                <option value="movie">Movie</option>
-                <option value="series">Series</option>
-                <option value="episode">Episode</option>
-              </Form.Control>
-            </Col>
-            <Col sm="3">
-              <Form.Label>Year of release. </Form.Label>
-              <Form.Control type="number" placeholder="Enter year" />
-            </Col>
-          </Row>
-        </Container>
+        <FormSearch
+          getFilmsInfo={getFilmsInfo}
+        />
       </Layout>
     );
   }
